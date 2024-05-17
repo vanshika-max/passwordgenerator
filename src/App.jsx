92f52a0dda9b882bc6004ea0777f802b39db288f
vanshika-max <vanshikaparjapati1@gmail.com>
2024-05-17 11:18:09 +0530

    padding

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <>
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-5 py-3 my-8 text-orange-500 bg-gray-700'>
         <h1 className='text-white text-center'>Password generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input
